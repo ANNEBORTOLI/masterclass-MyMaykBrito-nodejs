dd7e12fc1ffa7c6521eed7e5fdd7dfc94405d527
ANNEBORTOLI <annebortoli@gmail.com>
2021-08-16 12:19:09 -0300

    concluindo api

--- http/server.js
+++ http/server.js
@@ -1,26 +1,25 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
-const { extname } = require('path');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-http.createServer( (req, res) => {
 
-  const file = req.url === '/' ? 'index.html' : req.url;
-  const filePath = path.join(__dirname, 'public', file);
-  
-  //tradando o erro do favicon
-  const allowedFileTypes = ['.html', '.css', '.js'];
-  const allowed = allowedFileTypes.find(item => item == extname);
-  if (!allowed) return 
+http.createServer((req, res) => {
 
-  
-  fs.readFile(
-    filePath,
-    (err, content) => {
-      if(err) throw err
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public', file)
+    const extname = path.extname(filePath)
 
-      res.end(content)
-    }
-  )
-}).listen(5000, () => console.log("Servidor rodando na porta 5000..."));
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
 
+    if(!allowed) return
+
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if(err) throw err
+
+            res.end(content)
+        }
+    )
+}).listen(5000, () => console.log('Server is running'))
